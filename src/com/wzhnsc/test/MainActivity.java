00514bb2d4f96d8ab3e0ab75b6e074150dde87bd
wzhnsc <wzhnsc@163.com>
2015-10-28 19:13:44 +0800

    整理了一下代码风格和配置文件

--- src/com/wzhnsc/test/MainActivity.java
+++ src/com/wzhnsc/test/MainActivity.java
@@ -9,62 +9,60 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
-import com.example.test.R;
-
 public class MainActivity extends Activity {
     private EditText     edit;
 
-	static MainHandler mHandler;
+    static MainHandler mHandler;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         mHandler = new MainHandler();
 
-		edit = (EditText)findViewById(R.id.editText);
-		edit.setOnClickListener(new View.OnClickListener() {
+        edit = (EditText)findViewById(R.id.editText);
+        edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i("NdkUpdateUI", print("Look! I was updated!"));
             }
         });
-	}
+    }
 
-	// 对要调用的方法做本地声明
-	public static native String print(String nParam);
+    // 对要调用的方法做本地声明
+    public static native String print(String nParam);
 
-	static {
-		System.loadLibrary("print");
-	}
+    static {
+        System.loadLibrary("print");
+    }
 
-	public static void myCallbackFunc(String nMsg) {
-		Message tMsg = new Message();
-		Bundle  tBundle = new Bundle();
+    public static void myCallbackFunc(String nMsg) {
+        Message tMsg = new Message();
+        Bundle  tBundle = new Bundle();
 
-		tBundle.putString("CMD", nMsg);
-		tMsg.setData(tBundle);
+        tBundle.putString("CMD", nMsg);
+        tMsg.setData(tBundle);
 
-		mHandler.sendMessage(tMsg);
-	}
+        mHandler.sendMessage(tMsg);
+    }
 
-	class MainHandler extends Handler {
-		public MainHandler() {
-		}
+    class MainHandler extends Handler {
+        public MainHandler() {
+        }
 
-		public MainHandler(Looper L) {
-			super(L);
-		}
+        public MainHandler(Looper L) {
+            super(L);
+        }
 
-		@Override
+        @Override
         public void handleMessage(Message nMsg) {
-			super.handleMessage(nMsg);
+            super.handleMessage(nMsg);
 
-			Bundle tBundle = nMsg.getData();
-			String tCmd = tBundle.getString("CMD");
+            Bundle tBundle = nMsg.getData();
+            String tCmd = tBundle.getString("CMD");
 
-			MainActivity.this.edit.setText(tCmd);
-		}
-	}
+            MainActivity.this.edit.setText(tCmd);
+        }
+    }
 }
